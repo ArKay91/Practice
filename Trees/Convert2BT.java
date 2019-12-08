f8331b79b73e18b5136009a03704bc4a994a83f8
Rayanki Karthik <rayankikarthik@gmail.com>
2019-12-08 02:42:08 -0500

    Given Binary Tree Convert to full by removing nodes with only one child

---
+++ Trees/Convert2BT.java
@@ -0,0 +1,20 @@
+//Recall that a full binary tree is one in which each node is either a leaf node, or has two children. 
+//Given a binary tree, convert it to a full one by removing nodes with only one child.
+
+public class Convert2BT{
+
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val){
+            this.val = val;
+            this.left = null
+            this.right = null;
+        }
+    }
+
+    public TreeNode convert(TreeNode node){
+        
+    }
+}
