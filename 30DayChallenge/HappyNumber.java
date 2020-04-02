abf3f47260e9b631d96d95ecfad6c73633fce450
Rayanki Karthik <rayankikarthik@gmail.com>
2020-04-02 17:25:45 -0400

    Removing Space

--- 30DayChallenge/HappyNumber.java
+++ 30DayChallenge/HappyNumber.java
@@ -2,18 +2,29 @@ import java.util.*;
 
 public class HappyNumber{
     public boolean isHappy(int n){
-        Set<Integer> set = new HashSet<>();
+        // Set<Integer> set = new HashSet<>();
 
-        // there will be a loop if the sum doesn't end with 1.
-        while(!set.contains(n)){
-            set.add(n);
-            n = cumulativeSum(n);
-            if(n == 1){
-                return true;
-            }
-        }
+        // // there will be a loop if the sum doesn't end with 1.
+        // while(!set.contains(n)){
+        //     set.add(n);
+        //     n = cumulativeSum(n);
+        //     if(n == 1){
+        //         return true;
+        //     }
+        // }
 
-        return false;
+        // return false;
+        
+
+        // Without extra space
+        int fast = n;
+        int slow = n;
+        do{
+            slow = cumulativeSum(slow);
+            fast = cumulativeSum(cumulativeSum(fast));
+        }while(slow != fast);
+
+        return slow == 1 ? true : false;
     }
 
     private int cumulativeSum(int n){
