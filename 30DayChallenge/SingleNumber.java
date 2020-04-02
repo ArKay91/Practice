0eecc662a4c7ec415383659c819065e6192d24d8
Rayanki Karthik <rayankikarthik@gmail.com>
2020-04-02 17:36:05 -0400

    Single Number without space

--- 30DayChallenge/SingleNumber.java
+++ 30DayChallenge/SingleNumber.java
@@ -3,20 +3,26 @@ import java.util.*;
 public class SingleNumber {
     
     public int singleNumberIs(int[] nums){
-        int num = 0;
-        int sumTotal = 0;
-        Set<Integer> set = new HashSet<>();
-        for(int i=0; i<nums.length; i++){
-            set.add(nums[i]);
-            sumTotal += nums[i];
-        }
+        int singleNumber = 0;
+        //int num = 0;
+        //int sumTotal = 0;
+        // Set<Integer> set = new HashSet<>();
+        // for(int i=0; i<nums.length; i++){
+        //     set.add(nums[i]);
+        //     sumTotal += nums[i];
+        // }
         
-        int indTotal = 0;
-        for(int n : set){
-            indTotal += n;
+        // int indTotal = 0;
+        // for(int n : set){
+        //     indTotal += n;
+        // }
+
+        // return 2 * indTotal - sumTotal;
+        for(int num : nums){
+            singleNumber ^= num;
         }
 
-        return 2 * indTotal - sumTotal;
+        return singleNumber;
     }
 
     public static void main(String[] args){
